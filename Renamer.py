35b189c2347c2af4bde7ef5264920965fabcb8f4
Spoleshuck <spoleshuck@gmail.com>
2017-03-01 08:45:05 -0500

    Changed so that file names reflect excel values and not excel formulas.

--- Renamer.py
+++ Renamer.py
@@ -2,9 +2,9 @@ import logging
 from os import listdir
 from tkinter import *
 from tkinter.filedialog import askdirectory
-
 from tkinter.messagebox import showwarning
-from openpyxl import *
+
+from openpyxl import os, load_workbook, Workbook
 
 logger = logging.getLogger('Main')
 logger.setLevel(logging.DEBUG)
@@ -19,82 +19,86 @@ initial_directory = "C:/"
 
 
 class App:
-	def __init__(self, master):
-		self.frame = Frame(master)
-		self.frame.pack()
-		# master.iconbitmap('Icon.ico')
-
-		self.folder = ""
-
-		self.SelectButton = Button(self.frame, text="Select folder", command=self.scan_folder)
-		self.OpenButton = Button(self.frame, text="Open names file", command=self.open_name_file)
-		self.Apply = Button(self.frame, text="Apply Names", command=self.apply_names)
-
-		self.SelectButton.pack(side=TOP)
-		self.OpenButton.pack(side=TOP)
-		self.Apply.pack(side=TOP)
-
-	def scan_folder(self):
-		# Open dialog to ask for desired folder
-		self.folder = askdirectory(mustexist=True, initialdir=initial_directory)
-
-		logger.debug("Folder: {}".format(self.folder))
-		# Read file names in target
-
-		try:
-			files = listdir(self.folder)
-
-			try:
-				wb = Workbook()
-				ws = wb.active
-				ws.append(['File Name', 'New File Name', 'Extension', "{}".format(self.folder)])
-
-				for i in files:
-					logger.info("{}/{}".format(self.folder, i))
-					if os.path.isfile("{}/{}".format(self.folder, i)):
-						filename, file_extension = os.path.splitext(i)
-						logger.debug("{} - {}".format(filename, file_extension))
-						ws.append([filename, '', file_extension])
-
-				wb.save(name_file)
-
-			except IOError:
-				showwarning(title="File inaccessible", message="Please close names.xlsx")
-
-			self.open_name_file()
-
-		except FileNotFoundError:
-			pass
-
-		logger.debug("{} is closing".format(name_file))
-
-	@staticmethod
-	def open_name_file():
-		logger.debug("open_name_file ran")
-		os.startfile(name_file)
-
-	@staticmethod
-	def apply_names():
-		# try:
-		wb = load_workbook(name_file)
-		ws = wb.active
-		for row in ws.iter_rows(row_offset=1):
-			if row[1].value == ".":
-				# TODO: Delete file
-				pass
-			elif row[1].value == "":
-				pass
-			elif row[1].value is None:
-				pass
-			else:
-				os.chdir(ws["D1"].value)
-				print("dir: {}".format(os.getcwd()))
-				print("in: {}{}".format(row[0].value, row[2].value))
-				print("out: {}{}".format(row[1].value, row[2].value))
-				os.rename("{}{}".format(row[0].value, row[2].value),
-					"{}{}".format(row[1].value, row[2].value))
-
-			logger.debug("apply_names ran")
+    def __init__(self, master):
+        self.frame = Frame(master)
+        self.frame.pack()
+        # master.iconbitmap('Icon.ico')
+        
+        self.folder = ""
+        
+        self.SelectButton = Button(self.frame, text="Select folder", command=self.scan_folder)
+        self.OpenButton = Button(self.frame, text="Open names file", command=self.open_name_file)
+        self.Apply = Button(self.frame, text="Apply Names", command=self.apply_names)
+        
+        self.SelectButton.pack(side=TOP)
+        self.OpenButton.pack(side=TOP)
+        self.Apply.pack(side=TOP)
+    
+    def scan_folder(self):
+        # Open dialog to ask for desired folder
+        self.folder = askdirectory(mustexist=True, initialdir=initial_directory)
+        
+        logger.debug("Folder: {}".format(self.folder))
+        # Read file names in target
+        
+        try:
+            files = listdir(self.folder)
+            
+            try:
+                wb = Workbook()
+                ws = wb.active
+                ws.append(['File Name', 'New File Name', 'Extension', 'Folder'])
+                
+                for i in files:
+                    logger.info("{}/{}".format(self.folder, i))
+                    if os.path.isfile("{}/{}".format(self.folder, i)):
+                        filename, file_extension = os.path.splitext(i)
+                        logger.debug("{} - {}".format(filename, file_extension))
+                        ws.append([filename, '', file_extension, self.folder])
+                    
+                    if os.path.isdir("{}/{}".format(self.folder, i)):
+                        pass
+                        # TODO: Scan inner folders for files; Include line for folder and for each file
+                
+                wb.save(name_file)
+            # TODO: Grey out (and lock?) file format column
+            except IOError:
+                showwarning(title="File inaccessible", message="Please close {}".format(name_file))
+            
+            self.open_name_file()
+        
+        except FileNotFoundError:
+            pass
+        
+        logger.debug("{} is closing".format(name_file))
+    
+    @staticmethod
+    def open_name_file():
+        logger.debug("open_name_file ran")
+        os.startfile(name_file)
+    
+    @staticmethod
+    def apply_names():
+        # try:
+        wb = load_workbook(name_file, data_only=True)
+        ws = wb.active
+        for row in ws.iter_rows(row_offset=1):
+            if row[1].value == ".":
+                # TODO: Delete file
+                pass
+            elif row[1].value == "":
+                pass
+            elif row[1].value is None:
+                pass
+            else:
+                os.chdir(row[3].value)
+                print("dir: {}".format(os.getcwd()))
+                print("in: {}{}".format(row[0].value, row[2].value))
+                print("out: {}{}".format(row[1].value, row[2].value))
+                # os.rename("{}{}".format(row[0].value, row[2].value),
+                #           "{}{}".format(row[1].value, row[2].value))
+            
+            logger.debug("apply_names ran")
 
 
 root = Tk()
@@ -104,6 +108,6 @@ app = App(root)
 root.mainloop()
 
 try:
-	root.destroy()
+    root.destroy()
 except TclError:
-	pass
+    pass
--- setup.py
+++ setup.py
@@ -1,11 +1,6 @@
 from distutils.core import setup
-import py2exe
 
 setup(
-	windows=[
-		{
-			"script": "Renamer.py",
-			"icon_resources": [(0, "Icon.ico")]
-		}
-	], requires=['openpyxl','tkinter']
-)
+	windows=[{"script": "Renamer.py", "icon_resources": [(0, "Icon.ico")]}], requires=['openpyxl', 'tkinter'],
+	options={"py2exe": {"optimize": 2, "bundle_files": 2}},
+	data_files=[('images', ['Icon.ico'])])
